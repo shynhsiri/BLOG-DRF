accfee74e1ca5ab31a0211aab8808863297def2b
shynhsiri <shynhsiri@gmail.com>
2024-08-11 07:20:37 +0330

    user api functions implement

--- app/user/views.py
+++ app/user/views.py
@@ -1,1 +1,11 @@
-# noqa
+"""
+Views for the user API.
+"""
+from rest_framework import generics
+
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system."""
+    serializer_class = UserSerializer
