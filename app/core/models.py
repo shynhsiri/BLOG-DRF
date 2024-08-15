94cde1fcb8637d9e01b26679f801cb6443b4383c
shynhsiri <shynhsiri@gmail.com>
2024-08-15 21:31:38 +0330

    Implement Blog model

--- app/core/models.py
+++ app/core/models.py
@@ -1,4 +1,5 @@
 # Database models.
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     BaseUserManager,
@@ -44,3 +45,16 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Blog(models.Model):
+    """Blog Object."""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255, blank=False)
+    description = models.TextField(blank=False)
+
+    def __str__(self):
+        return self.title
