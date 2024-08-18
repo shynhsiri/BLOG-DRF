2761d83ca2deeafde8d2fde0dd26391a27bd5c71
shynhsiri <shynhsiri@gmail.com>
2024-08-18 15:54:00 +0330

    Private tests added

--- app/blog/tests/test_blog_api.py
+++ app/blog/tests/test_blog_api.py
@@ -40,3 +40,44 @@ class PublicBlogAPITests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
+
+class PrivateBlogAPITests(TestCase):
+    """Test authenticated API requests."""
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model.objects.create_user(
+            'user@example.com',
+            'testpass123',
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_retrive_blogs(self):
+        """Test retrieving a list of blogs."""
+        create_blog(user=self.user)
+        create_blog(user=self.user)
+
+        res = self.client.get(BLOGS_URL)
+
+        blogs = Blog.objects.all().order_by('-id')
+        serializer = BlogSerializer(blogs, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_blog_list_limited_to_user(self):
+        """Test blog list is limited to authenticated user."""
+        other_user = get_user_model().objects.create_user(
+            'other@example.com',
+            'testpass123',
+        )
+        create_blog(user=other_user)
+        create_blog(user=self.user)
+
+        res = self.client.get(BLOGS_URL)
+
+        blogs = Blog.objects.filter(user=self.user)
+        serializer = BlogSerializer(blogs, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
